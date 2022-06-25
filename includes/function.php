aac9573cbce786c53eee6769bc3f4c29244a2a48
leduong2903 <leduong290300@gmail.com>
2022-06-25 10:35:37 +0700

    send_mail

--- includes/function.php
+++ includes/function.php
@@ -1,7 +1,53 @@
-<?php 
-if(!defined('_INCODE')) die('Access deined...');
-function layout($layoutname='header',$data=[]){
-    if(file_exists(_WEB_PATH_TEMPLATE.'/layouts/'.$layoutname.'.php')){
-        require_once _WEB_PATH_TEMPLATE.'/layouts/'.$layoutname.'.php';
+<?php
+if (!defined('_INCODE')) die('Access deined...');
+
+use PHPMailer\PHPMailer\PHPMailer;
+use PHPMailer\PHPMailer\SMTP;
+use PHPMailer\PHPMailer\Exception;
+
+function layout($layoutname = 'header', $data = [])
+{
+    if (file_exists(_WEB_PATH_TEMPLATE . '/layouts/' . $layoutname . '.php')) {
+        require_once _WEB_PATH_TEMPLATE . '/layouts/' . $layoutname . '.php';
+    }
+}
+
+
+//* Gửi mail
+function sendMail($to, $subject, $content)
+{
+    $mail = new PHPMailer(true);
+
+    try {
+        //Server settings
+        $mail->SMTPDebug = SMTP::DEBUG_OFF;                      //Enable verbose debug output
+        $mail->isSMTP();                                            //Send using SMTP
+        $mail->Host       = 'smtp.gmail.com';                     //Set the SMTP server to send through
+        $mail->SMTPAuth   = true;                                   //Enable SMTP authentication
+        $mail->Username   = 'leduong290300@gmail.com';                     //SMTP username
+        $mail->Password   = 'fnswpxafiyzdymrf';                               //SMTP password
+        $mail->SMTPSecure = PHPMailer::ENCRYPTION_SMTPS;            //Enable implicit TLS encryption
+        $mail->Port       = 465;                                    //TCP port to connect to; use 587 if you have set `SMTPSecure = PHPMailer::ENCRYPTION_STARTTLS`
+
+        //Recipients
+        $mail->setFrom('leduong290300@gmail.com', 'Le Duong');
+        $mail->addAddress($to);     //Add a recipient
+
+
+        //Content
+        $mail->isHTML(true);                                  //Set email format to HTML
+        $mail->Subject = $subject;
+        $mail->Body    = $content;
+        //$mail->AltBody = 'This is the body in plain text for non-HTML mail clients';
+        $mail->SMTPOptions = array(
+            'ssl' => array(
+                'verify_peer' => false,
+                'verify_peer_name' => false,
+                'allow_self_signed' => true,
+            )
+        );
+        return $mail->send();
+    } catch (Exception $e) {
+        echo "Message could not be sent. Mailer Error: {$mail->ErrorInfo}";
     }
 }
